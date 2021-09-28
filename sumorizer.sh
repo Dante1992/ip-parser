f56fc1c1d6e21f156fd8c6cf9db7901cbdbe57ae
Dante1992 <hamletarm@gmail.com>
2021-09-28 04:51:20 -0700

    Delete sumorizer.sh

--- sumorizer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $1 && $2 && $3 =~ [0-9] ]]; then
-    echo "Your sum is equal to $SUM"
-else
-    echo "not all characters are numeric. Anyway your number's sum is $SUM"
-fi
-SUM=$(($1 + $2 + $3));
-echo $SUM
